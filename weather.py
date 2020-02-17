bf46bfa0c4342335a35d5a75c050b120561ae0ea
Tunde Oladipupo <simplytunde@gmail.com>
2020-02-17 11:24:58 -0600

    updated to handle error such as invalid API key

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 
 WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
-WEATHER_API_KEY = "73511d60d65c4b15be62c52a91039247"
+WEATHER_API_KEY = "73511d60d65c4b15be62c52a91039247" #obtain your API key from your account
 
 def getCityWeather(**kwargs):
     """Get weather for a city based on name or id.
@@ -13,14 +13,22 @@ def getCityWeather(**kwargs):
     """
     payload = {'appid':WEATHER_API_KEY}
     if 'city_id' in kwargs:
-        payload['city_id']=kwargs.get('city_id')
+        payload['id']=kwargs.get('city_id')
     elif 'city_name' in kwargs:
-        payload['city_query']=kwargs.get('city_name')
+        payload['q']=kwargs.get('city_name')
     else:
         raise Exception("Invalid Arguments: Please enter the city_id or city_name") 
     r = requests.get(WEATHER_API_URL, params=payload)
-    print(r.url)
-    return r.json()
+    if r.status_code == requests.codes.ok:
+        weather_description = ""
+        for desc in r.json()["weather"]:
+            weather_description += desc["description"] + ","
+        return weather_description[:-1] #remove last ,
+    else:
+        raise Exception(r.json()["message"])
+    return None
+
 
 if __name__ == "__main__":
-    print(getCityWeather(city_name="Los Angeles"))
+    city_name = "Los Angeles"
+    print(f"Weather In {city_name}: ",getCityWeather(city_name=city_name))
